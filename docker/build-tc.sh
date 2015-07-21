65740fd03aefa86b5e89129e82a957b53987bf8a
Andy Botting <andy@andybotting.com>
2015-07-22 09:06:09 +1000

    Update docker build script

--- docker/build-tc.sh
+++ docker/build-tc.sh
@@ -1,22 +1,26 @@
-docker build -t="working-${TEAMCITY_BUILDCONF_NAME}:${BUILD_NUMBER}" .
+#!/bin/bash
+
+NAME=${TEAMCITY_BUILDCONF_NAME%-docker}
+
+docker build -t="working-${NAME}:${BUILD_NUMBER}" .
 
 # Start container
-CONTAINER_ID=$(docker run -d working-${TEAMCITY_BUILDCONF_NAME}:${BUILD_NUMBER} /bin/bash)
+CONTAINER_ID=$(docker run -d working-${NAME}:${BUILD_NUMBER} /bin/bash)
 
 # Export/import container to flatten it
-docker export $CONTAINER_ID | docker import - dockreg.gutools.co.uk:8080/${TEAMCITY_BUILDCONF_NAME}:${BUILD_NUMBER}
+docker export $CONTAINER_ID | docker import - dockreg.gutools.co.uk:8080/${NAME}:${BUILD_NUMBER}
 
 # Kill the containter
 docker stop $CONTAINER_ID
 docker rm -f $CONTAINER_ID
 
 # Delete initial working image
-docker rmi working-${TEAMCITY_BUILDCONF_NAME}:${BUILD_NUMBER}
+docker rmi working-${NAME}:${BUILD_NUMBER}
 
-IMAGE_ID=$(docker images | awk "/${TEAMCITY_BUILDCONF_NAME} +${BUILD_NUMBER}/ {print \$3}")
-docker tag $IMAGE_ID dockreg.gutools.co.uk:8080/${TEAMCITY_BUILDCONF_NAME}:latest
+IMAGE_ID=$(docker images | awk "/${NAME} +${BUILD_NUMBER}/ {print \$3}")
+docker tag $IMAGE_ID dockreg.gutools.co.uk:8080/${NAME}:latest
 
-docker push dockreg.gutools.co.uk:8080/${TEAMCITY_BUILDCONF_NAME}
+docker push dockreg.gutools.co.uk:8080/${NAME}
 
 # Kill any running containers now that we're finished
 docker ps -a --no-trunc | grep 'Exit' | awk '{print $1}' | xargs -L 1 -r docker rm
