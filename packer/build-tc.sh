425dd51bd44d335ea4c11704945639001296e105
Andy Botting <andy@andybotting.com>
2015-07-22 12:26:00 +1000

    Might need to be in the right directory

--- packer/build-tc.sh
+++ packer/build-tc.sh
@@ -5,12 +5,10 @@
 set -e
 
 SCRIPTPATH=$(cd $(dirname $0); pwd -P)
+cd $SCRIPTPATH
 
 # Set up packer
 ${SCRIPTPATH}/setup-packer.sh
-
-# Copy our setup script
-cp ${SCRIPTPATH}/../scripts/setup.sh $SCRIPTPATH
 
 # try to get build branch
 if [ -n "${TEAMCITY_BUILD_PROPERTIES_FILE}" ]; then
