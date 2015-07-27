9480d6415bfadf0a04e74360af8af1694b289a94
Andy Botting <andy@andybotting.com>
2015-07-27 13:59:35 +1000

    Update local build script

--- build-local.sh
+++ build-local.sh
@@ -1,23 +1,28 @@
-#!/bin/bash -x
+#!/bin/bash
 
-DOCKER_NAME=parking-lot:latest
 ROOT_DIR=$(cd $(dirname "$0"); pwd)
+DOCKER_NAME=parking-lot:latest
 
 # Boot docker
-docker run -d -t -v $ROOT_DIR/sites:/etc/apache2/sites-enabled -p 18080:80 $DOCKER_NAME /usr/sbin/apache2 -DFOREGROUND
-[ $? -gt 0 ] && exit 1
-
-# Run test scripts
-$ROOT_DIR/testing/run-tests.sh
-TEST_RESULT=$?
+echo "Booting container..."
+docker run -d -t -v $ROOT_DIR/sites:/etc/apache2/sites-enabled -p 18080:80 $DOCKER_NAME /bin/bash -c 'source /etc/apache2/envvars; /usr/sbin/apache2 -DFOREGROUND' >/dev/null
+sleep 2
 
 CONTAINER_ID=$(docker ps | awk "/$DOCKER_NAME/ {print \$1}")
+if [ -z $CONTAINER_ID ]; then
+    echo "Can't find running container"
+    exit 1
+fi
 
-docker stop $CONTAINER_ID
-docker rm -f $CONTAINER_ID
+# Run test scripts
+echo "Running tests..."
+$ROOT_DIR/testing/run-tests.sh
+
+echo "Cleaning up..."
+CONTAINER_ID=$(docker ps | awk "/$DOCKER_NAME/ {print \$1}")
+
+docker stop $CONTAINER_ID >/dev/null
+docker rm -f $CONTAINER_ID >/dev/null
 
 # Optional: remove docker container after testing
 # docker rmi $DOCKER_NAME
-
-# If not successful, terminate here
-[ $TEST_RESULT -gt 0 ] && exit 1
