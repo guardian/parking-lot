7105bdfbae404b911e5b2ca76f50efaf96ff5820
Andy Botting <andy@andybotting.com>
2015-08-05 13:24:26 +1000

    Use sha256sum instead for update trigger

--- build-tc.sh
+++ build-tc.sh
@@ -20,10 +20,24 @@ fi
 # Run test scripts
 $ROOT_DIR/testing/run-tests.sh
 
-[ -d target ] && rm -rf target
-mkdir target
-cd target
+if [ $? -eq 0 ]; then
+    # Build working directory
+    [ -d $ROOT_DIR/build ] && rm -rf $ROOT_DIR/build
+    mkdir -p $ROOT_DIR/build/parking-lot
+    cd $ROOT_DIR/build
 
-cp -r ${ROOT_DIR}/sites .
-cp ${ROOT_DIR}/deploy/deploy.json .
-zip -rv artifacts.zip sites/ deploy.json
+    # Add build files
+    cp -r ${ROOT_DIR}/sites $ROOT_DIR/build/parking-lot/
+
+    echo "Building artifact..."
+    cd $ROOT_DIR/build
+    tar -zcvf $ROOT_DIR/build/parking-lot.tar.gz parking-lot/
+    git rev-parse HEAD > $ROOT_DIR/build/parking-lot-version.txt
+    sha256sum $ROOT_DIR/build/parking-lot.tar.gz | awk '{print $1}' > $ROOT_DIR/build/parking-lot.sha256
+    rm -fr $ROOT_DIR/build/parking-lot/
+
+    echo "Uploading to S3..."
+    aws s3 cp $ROOT_DIR/build/parking-lot.tar.gz s3://parking-lot/PROD/parking-lot.tar.gz
+    aws s3 cp $ROOT_DIR/build/parking-lot-version.txt s3://parking-lot/PROD/parking-lot-version.txt
+    aws s3 cp $ROOT_DIR/build/parking-lot.sha256 s3://parking-lot/PROD/parking-lot.sha256
+fi
--- scripts/parking-lot-sync.sh
+++ scripts/parking-lot-sync.sh
@@ -8,16 +8,15 @@ INSTANCE_PROFILE=$(wget -qO- http://instance-data//latest/meta-data/iam/security
 AWS_ACCESS_KEY_ID=$(wget -qO- http://instance-data/latest/meta-data/iam/security-credentials/${INSTANCE_PROFILE} | grep AccessKeyId | cut -d':' -f2 | sed 's/[^0-9A-Z]*//g')
 AWS_SECRET_ACCESS_KEY=$(wget -qO- http://instance-data/latest/meta-data/iam/security-credentials/${INSTANCE_PROFILE} | grep SecretAccessKey | cut -d':' -f2 | sed 's/[^0-9A-Za-z/+=]*//g')
 
-# Pull down our revision information
-[ -f /tmp/parking-lot-version.txt ] && rm -f /tmp/parking-lot-version.txt
-aws s3 --quiet --region $REGION cp s3://parking-lot/PROD/parking-lot-version.txt /tmp/parking-lot-version.txt
-BUILD_VERSION=$(cat /tmp/parking-lot-version.txt)
+[ -f /tmp/parking-lot.sha256 ] && rm -f /tmp/parking-lot.sha256
+aws s3 --quiet --region $REGION cp s3://parking-lot/PROD/parking-lot.sha256 /tmp/parking-lot.sha256
+BUILD_SUM=$(cat /tmp/parking-lot.sha256)
 
 # Test if we're running the latest
 [ -d /etc/gu ] || mkdir /etc/gu
-if [ -f /etc/gu/parking-lot-version.txt ]; then
-    CURRENT_VERSION=$(cat /etc/gu/parking-lot-version.txt)
-    [ "$BUILD_VERSION" == "$CURRENT_VERSION" ] && exit 0
+if [ -f /etc/gu/parking-lot.sha256 ]; then
+    CURRENT_SUM=$(cat /etc/gu/parking-lot.sha256)
+    [ "$BUILD_SUM" == "$CURRENT_SUM" ] && exit 0
 fi
 
 # Pull down new apache config
@@ -34,7 +33,7 @@ CONFIG_TEST=$(apachectl -t 2>&1)
 RESTART=$(service apache2 restart 2>&1)
 
 # Set running build version
-mv /tmp/parking-lot-version.txt /etc/gu/parking-lot-version.txt
+mv /tmp/parking-lot.sha256 /etc/gu/parking-lot.sha256
 
 # Also copy the git revision to our healthcheck path
-cp /etc/gu/parking-lot-version.txt /var/www/html/healthcheck
+cp /etc/gu/parking-lot.sha256 /var/www/html/healthcheck
